1134816e932bc0df878a58c0db70f58fd80aeb57
TinHead <razlix77@gmail.com>
2024-07-12 14:45:08 +0300

    serial wip

--- poco-ros2/main.go
+++ poco-ros2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"machine"
 	"time"
@@ -69,20 +70,96 @@ func initUart() machine.UART {
 	return *uart0
 }
 
+func handleMotors(packet []byte) error {
+	drive := initMotors()
+	if len(packet) > 1 {
+		switch packet[1] {
+		case 102:
+			// forward
+			fmt.Println("Got fw command!")
+			drive.mot1.Forward()
+			drive.mot2.Forward()
+		case 98:
+			// backward
+			drive.mot1.Backward()
+			drive.mot2.Backward()
+		case 115:
+			//stop
+			drive.mot1.Stop()
+			drive.mot2.Stop()
+		case 108:
+			//left
+		case 114:
+			//right
+		default:
+			return errors.New("Wrong motor packet format:" + string(packet))
+
+		}
+	} else {
+		return errors.New("Packet is missing command")
+	}
+	return nil
+}
+
+func handlePacket(packet []byte, uart machine.UART) error {
+	// packet format [type,data] where type is packet type and data nil or int value
+	switch packet[0] {
+	case 109:
+		// motor control - one or two values:
+		// forward - f 1-100
+		// backward - b 1-100
+		// stop s
+		// left - l 1-100
+		// right - r 1-100
+		// turn left a 1-100
+		// turn right d 1-100
+		err := handleMotors(packet)
+		if err != nil {
+			return err
+		}
+	case 101:
+		// echo request no value
+	case 98:
+		// battery level no value
+	case 99:
+		// custom request?
+	default:
+		return errors.New("Don't know how to handle packet: " + string(packet[:]))
+	}
+	return nil
+}
+
 // handle serial commands
 func handleComms(uart machine.UART) {
 	fmt.Println("Starting comms ..")
+	packet := []byte{}
 	for {
 		if uart.Buffered() > 0 {
 			rcv, err := uart.ReadByte()
 			if err != nil {
 				fmt.Println("Error while receiving: ", err)
 			}
-			if rcv == 0 {
-				fmt.Println("nothing received")
-			} else {
-				fmt.Println("received: ", rcv)
+			switch rcv {
+			case 13:
+				fmt.Println("End of packet, contains:", packet)
+				/// handle the complete packet
+				if packet != nil {
+					err := handlePacket(packet, uart)
+					if err != nil {
+						fmt.Println(err)
+					}
+				}
+				packet = nil
+			default:
+				fmt.Println("Got ", rcv)
+				packet = append(packet, rcv)
+				fmt.Println("Packet so far: ", packet)
 			}
+			// if rcv == 0 {
+			// 	fmt.Println("nothing received")
+			// } else {
+			// 	fmt.Println("received: ", rcv)
+			// }
 		}
 
 		time.Sleep(time.Millisecond * 1)
@@ -91,11 +168,11 @@ func handleComms(uart machine.UART) {
 
 func main() {
 	fmt.Println("Ready")
-	test := initMotors()
-	test.mot1.Configure()
-	test.mot1.Forward()
-	test.mot2.Configure()
-	test.mot2.Forward()
+	// test := initMotors()
+	// test.mot1.Configure()
+	// test.mot1.Forward()
+	// test.mot2.Configure()
+	// test.mot2.Forward()
 
 	uart := initUart()
 	go handleComms(uart)
